9391f6f8ee257767cc7ac60eb363133d29ba2220
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2023-10-26 10:52:22 +0200

    Animaciones

--- ProyectoJuego2D/Assets/Scripts/Jugador.cs
+++ ProyectoJuego2D/Assets/Scripts/Jugador.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jugador : MonoBehaviour
+public class PlayerFrog : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+    float horizontal;
+    float speed = 5f;
+    float salto;
+    bool isRunning = false;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("isRunning", isRunning);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0)
+            isRunning = true;
+        else
+            isRunning = false;
+        animator.SetBool("isRunning", isRunning);
+        Vector2 velocity = rb2d.velocity;
+        velocity.x = horizontal * speed;
+        rb2d.velocity = velocity;
+
+        salto = Input.GetAxis("Jump");
+        Debug.Log("Salto " + salto);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.AddForce(new Vector2(0, 20));
+        }
     }
 }
+
