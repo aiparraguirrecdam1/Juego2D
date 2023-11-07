4e3e977da0eac679f2e1c75434d45fe3db20f8f7
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2023-11-07 22:02:40 +0100

    Creando nueva escena y probando saltos en el jugador

--- ProyectoJuego2D/Assets/Scripts/Jugador.cs
+++ ProyectoJuego2D/Assets/Scripts/Jugador.cs
@@ -7,7 +7,7 @@ public class PlayerFrog : MonoBehaviour
     Rigidbody2D rb2d;
     float horizontal;
     float speed = 5f;
-    float salto;
+    public float fuerzaSalto;
     bool isRunning = false;
     Animator animator;
 
@@ -33,11 +33,21 @@ public class PlayerFrog : MonoBehaviour
         velocity.x = horizontal * speed;
         rb2d.velocity = velocity;
 
-        salto = Input.GetAxis("Jump");
+        /*salto = Input.GetAxis("Jump");
         Debug.Log("Salto " + salto);
         if (Input.GetKey(KeyCode.Space))
         {
             rb2d.AddForce(new Vector2(0, 20));
+        }*/
+
+        ProcesarSalto();
+    }
+
+    void ProcesarSalto()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
         }
     }
 }
